9b4b26a8bc6be0e02e771ef9093fee6e4a1e5bf5
WillianTambori <msn.willian@gmail.com>
2023-02-24 11:11:03 -0300

    array

---
+++ ArrayProduto/Produto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayProduto
+{
+    public class Produto
+    {
+        //Crie os atributos e métodos desta classe
+        //na Main() crie um vetor de 3 elementos
+        //cadastre-os apresente-os
+        //Calcule e apresente a média de preços maiores que 100 reais
+    }
+}
